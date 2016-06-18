e702c5f3cd3ae1d75998b3f11d5acf5bf5a98cee
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2016-06-17 23:04:22 -0400

    Add comments to multithread example test for clarity

--- tests/multithread-example.rs
+++ tests/multithread-example.rs
@@ -58,10 +58,38 @@ fn primary_example() {
     }
 }
 
+// Create a tcp client that writes some data and expects to receive it back.
+// This client uses standard blocking sockets, and doesn't use the poller/registrar at all.
+//
+// This client drives the flow of the test. Lines of the client will be numbered to correspond with
+// sections of the poller and worker threads to describe what is happening. This should allow
+// understanding the asserts and flow of the test in context.
+fn run_client() {
+    // 1) Connect to the non-blocking listening socket registered with the poller. The poller will
+    // accept the socket and forward it to the worker who will register it with some user data.
+    let mut sock = TcpStream::connect(IP).unwrap();
+
+    // 2 + 3) Write a single line of data. This data will cause a read event in the poller, which gets
+    // forwarded to the worker who will read it. The worker will then register the socket again with
+    // a write event, so that it can echo the data back. This write event gets forwarded to the
+    // worker and it writes the data on the socket.
+    sock.write_all(DATA.as_bytes()).unwrap();
+
+    // 4) At this point the poller has received the write event and forwarded it to the worker
+    // which has written the line of data. This data is received and it is checked that it is indeed
+    // an echo of the original data that was sent.
+    let mut buf = vec![0; DATA.len()];
+    sock.read_exact(&mut buf).unwrap();
+    assert_eq!(DATA, str::from_utf8(&buf).unwrap());
+}
+
 /// This thread runs the poller and forwards notifications to a worker thread.
 fn run_poller(mut poller: Poller<Option<LineReader>>, tx: Sender<PollEvent>) {
 
-    // Wait for a connection, and ensure we get one. We started listening in the main thread.
+    // 1) Wait for a connection, and ensure we get one. We started listening in the main thread. The
+    // client has connected so we only get a single read event. There was no user data registered
+    // for the listening socket. The connection gets forwarded to the worker that registers it along
+    // with some user data for a read event.
     let mut notifications = poller.wait(5000).unwrap();
     assert_eq!(1, notifications.len());
     let notification = notifications.pop().unwrap();
@@ -72,7 +100,9 @@ fn run_poller(mut poller: Poller<Option<LineReader>>, tx: Sender<PollEvent>) {
     // Accept the connection and forward it to the worker
     accept(&registration.socket, &tx);
 
-    // Wait for a read event signalling data from the client
+    // 2) Wait for a read event signalling data from the client. Only one line of data from a single
+    // client was sent, so there is only one read notification. There is some user data registered
+    // and the notification will be forwarded to the worker.
     let mut notifications = poller.wait(5000).unwrap();
     assert_eq!(1, notifications.len());
     let notification = notifications.pop().unwrap();
@@ -83,7 +113,10 @@ fn run_poller(mut poller: Poller<Option<LineReader>>, tx: Sender<PollEvent>) {
     let msg = PollEvent::Notification(notification);
     tx.send(msg).unwrap();
 
-    // Wait for a write event signalling a response to the client from the worker
+    // 3) The worker will read the data off the socket after it receives the read notification, and
+    // register a write event on the same socket. The write socket buffer is empty because it's the
+    // first time writing to the stream, so a write event becomes available immediately after
+    // polling, and forwarded to the worker who writes data on the socket.
     let mut notifications = poller.wait(5000).unwrap();
     assert_eq!(1, notifications.len());
     let notification = notifications.pop().unwrap();
@@ -94,14 +127,17 @@ fn run_poller(mut poller: Poller<Option<LineReader>>, tx: Sender<PollEvent>) {
     let msg = PollEvent::Notification(notification);
     tx.send(msg).unwrap();
 
-    // We should be done here. So poll and wait for a timeout.
+    // 4) We should be done here. So poll and wait for a timeout.
     let notifications = poller.wait(1000).unwrap();
     assert_eq!(0, notifications.len());
 }
 
-/// This thread registers sockets and receives notifications from the poller when they are ready
+// This thread registers sockets and receives notifications from the poller when they are ready
 fn run_worker(registrar: Registrar<Option<LineReader>>, rx: Receiver<PollEvent>) {
-    // Receive a notification about a connection from a client, and register for read events
+
+    // 1) A connection has been accepted by the poller and forwarded to this worker thread, which
+    // takes ownership of the socket and registers it for a read event along with a LineReader as
+    // user data.
     if let PollEvent::NewSock(sock, _) = rx.recv().unwrap() {
         registrar.register(Socket::TcpStream(sock),
                            Event::Read,
@@ -112,6 +148,9 @@ fn run_worker(registrar: Registrar<Option<LineReader>>, rx: Receiver<PollEvent>)
 
     let mut outgoing = String::new();
 
+    // 2) Data was received on the socket from the client, the read event was handled by the poller
+    // and forwarded to this worker.
+    //
     // Receive notification that there is data to be read, read the data, and decode it
     // Note that it's small data that shouldn't fill our buffers and will be in one message
     if let PollEvent::Notification(notification) = rx.recv().unwrap() {
@@ -146,7 +185,8 @@ fn run_worker(registrar: Registrar<Option<LineReader>>, rx: Receiver<PollEvent>)
         assert!(false);
     }
 
-    // Receive notification that the socket is available for writing
+    // 3) The socket was available for writing, and the notification was forwarded from the poller.
+    // This worker receives the notification and proceeds to echo back the read data.
     if let PollEvent::Notification(notification) = rx.recv().unwrap() {
         assert_eq!(notification.event, Event::Write);
 
@@ -167,16 +207,8 @@ fn run_worker(registrar: Registrar<Option<LineReader>>, rx: Receiver<PollEvent>)
     } else {
         assert!(false);
     }
-}
 
-// Create a tcp client that writes some data and expects to receive it back.
-// This client uses standard blocking sockets, and doesn't use the poller/registrar at all.
-fn run_client() {
-    let mut sock = TcpStream::connect(IP).unwrap();
-    sock.write_all(DATA.as_bytes()).unwrap();
-    let mut buf = vec![0; DATA.len()];
-    sock.read_exact(&mut buf).unwrap();
-    assert_eq!(DATA, str::from_utf8(&buf).unwrap());
+    // 4) The data was sent, and we are done here.
 }
 
 /// Setup a listen socket in non-blocking mode and register it for Read Events
