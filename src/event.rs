0c33ba28bdb09c1cdb14ce250f8b618605f9b251
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2016-09-19 15:54:16 -0400

    Add readable() + writable() interface for Event

--- src/event.rs
+++ src/event.rs
@@ -4,3 +4,19 @@ pub enum Event {
     Write,
     Both
 }
+
+impl Event {
+    pub fn readable(&self) -> bool {
+        match *self {
+            Event::Read | Event::Both => true,
+            _ => false
+        }
+    }
+
+    pub fn writable(&self) -> bool {
+        match *self {
+            Event::Write | Event::Both => true,
+            _ => false
+        }
+    }
+}
